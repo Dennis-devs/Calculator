a7b138af88a2deb6f5dfa6061e70521245901a86
Dennis-devs <dennis.ogoll@gmail.com>
2022-08-25 12:17:17 +0300

    Make the square and pi buttons work

--- calc2.js
+++ calc2.js
@@ -1,26 +1,23 @@
-/*Equations*/ 
+/*EQUATIONS*/ 
 let screen;
 screen = document.querySelector(".screen");
 let disp;
 disp = screen.innerHTML
 let add = function(a, b){
-    if(b == ""){return "Syntax Error"}
-    else if(a == ""){return disp}
-    else {return parseInt(a) + parseInt(b)} 
+    return parseFloat(a) + parseFloat(b)
 }
 let subtract = function(a, b){
-    if(b == ""){return "Syntax Error"}
-    else if(a == ""){return disp}
-    else {return parseInt(a) - parseInt(b)} 
+     return parseFloat(a) - parseFloat(b)
 }
 let multiply = function(a, b){
-    if(a == "" || b == ""){return "Syntax Error"}
-    else {return parseInt(a) * parseInt(b)}
+    return parseFloat(a) * parseFloat(b)
 }
 let divide = function(a, b){
-    if(a == "" || b == ""){return "Syntax Error"}
-    else if (parseInt(b) == 0){return "...To Infinity and beyond"}
-    else{return parseInt(a) / parseInt(b)}   
+    if (parseFloat(b) == 0){return "...To Infinity and Beyond"}
+    else{return parseFloat(a) / parseFloat(b)}   
+}
+let Squaring = function(a){
+    return parseFloat(a) * parseFloat(a)
 }
 
 /*Populate screen when empty and when not*/
@@ -42,88 +39,161 @@ let num = document.querySelectorAll(".print");
         
         }
 
-/*Save first number and operator selected*/
-    disp = screen.innerHTML
-    /*let arr = disp.match(/\d+/g)*/
+        let operate = function(a, b, operator){
+            if(operator == '+'){
+                console.log(add(a,b))
+                return add(a, b)
+            }
+            else if(operator == '-'){
+                console.log(subtract(a,b))
+                return subtract(a, b)
+            }
+            else if(operator == 'x'){
+                console.log(multiply(a,b))
+                return multiply(a, b)
+            }
+            else 
+            console.log(divide(a,b))
+                return divide(a, b)
+        }
+        /*Save first number and operator selected*/
+        disp = screen.innerHTML
+        /*let arr = disp.match(/\d+/g)*/
 
-    let regex = /\d+(?=\D)?/g
-    let nums = disp.match(regex)
-    console.log(nums)
-    console.log(nums.slice(-1).join())
-    let regex2 = /\D(?=\d)?/g
-    let signs = disp.match(regex2)
-    console.log(signs)
-    console.log(signs.slice(-1).join())
+        /*let regex = /\d+.?\d+?(?=\D)?/g*/
+        let regex = /√?(\d*[.])?\d+/g;
+        let nums = disp.match(regex);
+        console.log(nums)
+        console.log(nums.slice(-1).join())
+        /*let regex2 = /\D(?=\d)?[^.]/g*/
+        let regex2 = /[^.√\d\u00B2]/g;
+        let signs = disp.match(regex2);
+        console.log(signs)
+        console.log(signs.slice(-1).join())
+        
+        let regex3 = /^[x÷]/;
+        let regex4 = /\d+/
+        let disp2;
+        let operator;
+        let equal;
+        
+        function allSigns(arrSign){
+            return arrSign.every(sign => {
+                return isNaN(sign)
+            })
+        }
     
-
-    let operate = function(a, b, operator){
-        if(operator == '+'){
-            console.log(add(a,b))
-            return add(a, b)
+        equal = document.querySelector(".equal");
+        disp2 = document.querySelector(".screenB");
+    
+        if(isNaN(disp.split("").slice(-1).join("")) == true || regex3.test(disp.split("")[0]) == true || regex4.test(screen.innerHTML) == false){
+            equal.addEventListener("click", function(){
+                disp2.innerHTML = "Syntax Error"
+                return disp2.innerHTML
+            })
         }
-        else if(operator == '-'){
-            console.log(subtract(a,b))
-            return subtract(a, b)
+        else if(disp.includes("\u00B2") == true){
+            equal.addEventListener("click", function(){
+                disp2.innerHTML = operate(disp2.innerHTML, nums.slice(-1).join(""), signs.slice(-1).join("") )
+            })
+            
         }
-        else if(operator == 'x'){
-            console.log(multiply(a,b))
-            return multiply(a, b)
-        }
-        else 
-        console.log(divide(a,b))
-            return divide(a, b)
-    }
-
-    let disp2 ;
-    let operator;
-    let equal;
-
-    disp2 = document.querySelector(".screenB")
-    if (nums.length === 2){
-        equal = document.querySelector(".equal")
-        equal.addEventListener("click", function(){
-            disp2.innerHTML = operate(nums[0], nums[1], signs[0]) 
-            return disp2.innerHTML   
-    })
+        else if(nums.length == 2){
+            equal.addEventListener("click", function(){
+                disp2.innerHTML = operate(nums[0], nums[1], signs[0])
+                return disp2.innerHTML
+        })
         operator = document.querySelectorAll(".operator")
         operator.forEach(item => item.addEventListener("click", function(){
             disp2.innerHTML = operate(nums[0], nums[1], signs[0])
             return disp2.innerHTML
-    }))
+        }))
     }
     
     else if(nums.length > 2){
-        equal = document.querySelector(".equal")
         equal.addEventListener("click", function(){
-            disp2.innerHTML = operate(disp2.innerHTML, nums.slice(-1).join(), signs.slice(-1).join())
+            disp2.innerHTML = operate(disp2.innerHTML, nums.slice(-1).join(""), signs.slice(-1).join(""))
+            console.log(disp2.innerHTML)
             return disp2.innerHTML
-    })
+        })
+        operator = document.querySelectorAll(".operator")
+        operator.forEach(item => item.addEventListener("click", function(){
+            disp2.innerHTML = operate(disp2.innerHTML, nums.slice(-1).join(""), signs.slice(-1).join(""))
+            console.log(disp2.innerHTML)
+            return disp2.innerHTML
+        }))
+    }
     
-    operator = document.querySelectorAll(".operator")
-    operator.forEach(item => item.addEventListener("click", function(){
-        disp2.innerHTML = operate(disp2.innerHTML, nums.slice(-1).join(), signs.slice(-1).join())
-        return disp2.innerHTML
-    }))
-}
-
-        
+    /*CLEAR*/
+    disp2 = document.querySelector(".screenB")
+    let clear = document.querySelector(".clear")
+    clear.addEventListener("click", function(){
+        screen.innerHTML = ""
+        disp2.innerHTML = parseInt("0")
+        console.clear()
+        nums.splice(0, nums.length, 0, 0)
+        signs.splice(0, signs.length, "+")
+    })
 })) 
 
 
-let del = document.querySelector(".del")
+/*DELETE*/
+let del = document.querySelector(".del");
 del.addEventListener("click", function(){
-let arr = screen.innerHTML.split('')
-arr.splice(-1)
-screen.innerHTML = arr.join('')
+    let arr = screen.innerHTML.split('');
+    arr.splice(-1)
+    screen.innerHTML = arr.join('')
 })
 
-let disp2;
-disp2 = document.querySelector(".screenB")
+ /*Pi*/
+ screen = document.querySelector(".screen");
+ let pi = document.querySelector(".pi")
+ pi.addEventListener("click", function(){
+ if(screen.innerHTML != ""){
+     /*let val = parseFloat(operate(22, 7, "/")).toFixed(0)*/
+     let val = pi.value
+     screen.insertAdjacentText("beforeend", val)   
+ }
+ else {
+     /*let val = parseFloat(operate(22, 7, "/")).toFixed(0);*/
+     let val = pi.value
+     screen.innerHTML = val;
+ }
+ })
+
+ /*SQUARE ROOT */
+let Root = document.querySelector(".Root")
+Root.addEventListener("click", function(){
+    if(screen.innerHTML != ""){
+        screen.insertAdjacentText("beforeend", Root.innerHTML)   
+    }
+    else{
+        screen.innerHTML = Root.innerHTML
+    }
+})
+
+/*SQUARE*/
+let regex5 = /(\d*\.)?\d+/;
+equal = document.querySelector(".equal");
+let disp2 = document.querySelector(".screenB");
+let square = document.querySelector(".square")
+square.addEventListener("click", function(){
+    let squared = "\u00B2"
+    if(screen.innerHTML != ""){
+        screen.insertAdjacentText("beforeend", squared)   
+    }
+    disp2.innerHTML = Squaring(parseFloat(screen.innerHTML.match(regex5).join("")))
+})
+
+
+/*disp2 = document.querySelector(".screenB")
 let clear = document.querySelector(".clear")
 clear.addEventListener("click", function(){
     screen.innerHTML = ""
     disp2.innerHTML = ""
-})
+    
+})*/
+
 
 /*num.forEach(item => item.addEventListener("click", function(){
     let vals = item.innerHTML
